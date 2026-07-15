42eb16edae7dfa00158afbfafba530a662c5efd2
Vio <112751402+Genta-ta@users.noreply.github.com>
2026-07-15 17:33:41 +0000

    Pending changes exported from your codespace

---
+++ android-app/app/src/main/java/id/fc/pratv/MainActivity.kt
@@ -0,0 +1,17 @@
+package id.fc.pratv
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonStart).setOnClickListener {
+            Toast.makeText(this, "Selamat datang di pranemanTV!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
